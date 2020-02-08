5f64de36f77f823af7df304f973fda48ccf38cfe
Vikas Shukla <shuk.vk@gmail.com>
2020-02-08 13:50:25 -0800

    Adds folder

---
+++ realProxy/server/index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const $ = require("jquery");
+const axios = require('axios');
+const bodyParser = require('body-parser');
+const app = express()
+const port = 3000
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// console.log($);
+
+app.use(express.static('../public'));
+
+app.get('/data/0', (req, res) => {
+    axios.get('http://54.153.109.129:3000/data/1')
+    .then((innerRes) => {
+      console.log(innerRes.data);
+      res.json(innerRes.data);
+    })
+    .catch((err) => {console.log(err)})
+  });
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
