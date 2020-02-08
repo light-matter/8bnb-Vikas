0076cdc29518fad614d4dfd801a23153d692a5c9
Vikas Shukla <shuk.vk@gmail.com>
2020-02-08 13:54:47 -0800

    Changes port

--- realProxy/server/index.js
+++ realProxy/server/index.js
@@ -3,7 +3,7 @@ const $ = require("jquery");
 const axios = require('axios');
 const bodyParser = require('body-parser');
 const app = express()
-const port = 3000
+const port = 3001
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
